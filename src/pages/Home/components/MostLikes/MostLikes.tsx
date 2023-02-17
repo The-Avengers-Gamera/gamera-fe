bf0696cb1928c0f7256ada45cf43649984fc59a9
RoRobinh98 <houjiazhe98@163.com>
2023-02-17 07:33:56 +0700

    like_card_creat

--- src/pages/Home/components/MostLikes/MostLikes.tsx
+++ src/pages/Home/components/MostLikes/MostLikes.tsx
@@ -1,25 +1,21 @@
+import { v4 as uuid } from 'uuid';
 import { Card } from '@mui/material';
 import React from 'react';
 import GeneralContainer from '../GeneralContainer';
 import ContainerHeader from '../ContainerHeader';
+// import LikeCard from '../MostLikes/MostLikesCom/LikeCard';
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const cards = ['card', 'card', 'card', 'card', 'card', 'card'];
 const MostLikes = () => (
   <GeneralContainer
     sx={{ padding: '0.5rem 3rem 3rem' }}
     header={<ContainerHeader title="MOST LIKED" />}
+    padding={3}
     laptop={4}
     mobile={6}
-    divider
   >
     {cards.map((card) => (
-      // replace your card here
-      <Card
-        sx={{ height: 30 }}
-        key={card}
-      >
-        CARD SLOT {card}
-      </Card>
+      <Card key={uuid()}>CARD SLOT {card}</Card>
     ))}
   </GeneralContainer>
 );
--- src/pages/Home/components/MostLikes/MostLikesCom/LikeCard.tsx
+++ src/pages/Home/components/MostLikes/MostLikesCom/LikeCard.tsx
@@ -1,117 +1,16 @@
-import FavoriteRoundedIcon from '@mui/icons-material/FavoriteRounded';
-import ChatRoundedIcon from '@mui/icons-material/ChatRounded';
+import { Box, Typography } from '@mui/material';
 import React from 'react';
-import styled from 'styled-components';
+// import style from './index.module.css';
 
-const Container = styled.div`
-  //width: 565px;
-  height: 65px;
-  font-family: Poppins;
-  display: flex;
-  align-items: center;
-`;
-
-const Cover = styled.div`
-  height: 50px;
-  width: 50px;
-  margin-left: 7.5px;
-  display: inline-block;
-
-  & img {
-    width: 50px;
-    height: 100%;
-  }
-`;
-
-const ArticleOverview = styled.div`
-  font-size: 14px;
-  font-weight: 700;
-  margin-left: 7.5px;
-  margin-right: 7.5px;
-  display: inline-block;
-  width: 400px;
-  & .Tile_container {
-    & span {
-      margin-left: 5px;
-
-      font-size: 14px;
-      font-weight: bold;
-    }
-  }
-  & .Comment_container {
-    & span {
-      margin-left: 5px;
-      font-size: 14px;
-      font-weight: bold;
-    }
-  }
-`;
-
-const Like = styled.div`
-  display: flex;
-  align-items: center;
-  display: inline-block;
-  justify-content: center;
-  align-items: center;
-
-  & .LikeNum_container {
-    display: flex;
-    align-items: center;
-    & span {
-      margin-left: 5px;
-      font-size: 14px;
-      font-weight: bold;
-    }
-  }
-`;
-
-const CommentIcon = styled(ChatRoundedIcon)`
-  width: 12.83px;
-  height: 11.96px;
-`;
-
-const LikeIcon = styled(FavoriteRoundedIcon)`
-  color: red;
-  width: 13.33px;
-  height: 12.33px;
-`;
-
-type LikeCardProps = {
-  coverUrl: string;
-  title: string;
-  commNum: string;
-  likeNum: string;
-};
-
-const LikeCard = ({ coverUrl, title, commNum, likeNum }: LikeCardProps) => {
-  return (
-    <Container>
-      <Cover>
-        {/* fake href */}
-        <a href="https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/HEAD/docs/rules/anchor-is-valid.md">
-          <img
-            src={coverUrl}
-            alt=""
-          />
-        </a>
-      </Cover>
-      <ArticleOverview>
-        <div className="Tile_container">
-          <span>{title}</span>
-        </div>
-        <div className="Comment_container">
-          <CommentIcon />
-          <span>{commNum}</span>
-        </div>
-      </ArticleOverview>
-      <Like>
-        <div className="LikeNum_container">
-          <LikeIcon />
-          <span>{likeNum}</span>
-        </div>
-      </Like>
-    </Container>
-  );
-};
+// @ts-ignore
+const LikeCard = () => (
+  <div className="LikeCard">
+    This is like card
+    <div className="articleCover">This is cover</div>
+    <Typography className="articleTitle">This is article title</Typography>
+    <Box className="commentNumber">This is comment number</Box>
+    <Box className="likeNumber">This is like number</Box>
+  </div>
+);
 
 export default LikeCard;
