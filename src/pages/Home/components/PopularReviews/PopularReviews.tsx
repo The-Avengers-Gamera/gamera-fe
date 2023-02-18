24fe0de2b9e5ac83e8bd5940cbef31f05adecd15
XUeshuo Li <spoonlee24k@gmail.com>
2023-02-18 18:16:08 +1100

    fix review card useState() bug

--- src/pages/Home/components/PopularReviews/PopularReviews.tsx
+++ src/pages/Home/components/PopularReviews/PopularReviews.tsx
@@ -1,5 +1,6 @@
 // import { Card } from '@mui/material';
-// import React, { useState } from 'react';
+// import React from 'react';
+import { useState } from 'react';
 
 import ReviewCard from './components/ReviewCard';
 
@@ -7,7 +8,8 @@ import GeneralContainer from '../GeneralContainer';
 import ContainerHeader from '../ContainerHeader';
 
 // const cards = [1, 2, 3, 4, 5, 6, 7];
-// mock data sent from backend ------------------------------------------
+
+// mock data sent from backend ------------------------------------------------------------
 type ReviewItemType = {
   imgUrl: string;
   title: string;
@@ -15,6 +17,7 @@ type ReviewItemType = {
   publishTime: string;
 };
 
+// const initialState: ReviewItemType[] = [];
 const initialState: ReviewItemType[] = [];
 
 const mockReviewItem = {
@@ -24,24 +27,26 @@ const mockReviewItem = {
   authorName: 'LUKE REILLY',
   publishTime: '1 day ago',
 };
-for (let i = 1; i <= 8; ) {
+for (let i = 0; i < 8; ) {
   initialState.push(structuredClone(mockReviewItem));
+  mockReviewItem.title = `The Last Of Us Part 1 Review ${i}`; // mock data: make title unique
   i += 1;
 }
 
-// TODO: state is needed for dynamic page
-// const [reviewList, setReviewList] = useState<ReviewItemType[]>(initialState);
-// ------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------
 
-const PopularReviews = () => (
-  <GeneralContainer
-    sx={{ padding: '0.5rem 3rem 3rem' }}
-    header={<ContainerHeader title="POPULAR REVIEWS" />}
-    laptop={3}
-    mobile={4}
-    divider
-  >
-    {/* {cards.map((card) => (
+const PopularReviews = () => {
+  const [reviewList] = useState(initialState);
+
+  return (
+    <GeneralContainer
+      sx={{ padding: '0.5rem 3rem 3rem' }}
+      header={<ContainerHeader title="POPULAR REVIEWS" />}
+      laptop={3}
+      mobile={4}
+      divider
+    >
+      {/* {cards.map((card) => (
       // replace your card here
       <Card
         sx={{ height: 250 }}
@@ -51,18 +56,19 @@ const PopularReviews = () => (
       </Card>
     ))} */}
 
-    {/* TODO: replace initialState below with reviewList, which is a state */}
-    {initialState.map((review) => {
-      return (
-        <ReviewCard
-          key={`${review.title} + ${review.authorName}`}
-          coverUrl={review.imgUrl}
-          title={review.title}
-          authorName={review.authorName}
-          publishTime={review.publishTime}
-        />
-      );
-    })}
-  </GeneralContainer>
-);
+      {/* TODO: replace initialState below with reviewList, which is a state */}
+      {reviewList.map((review) => {
+        return (
+          <ReviewCard
+            key={`${review.title}`}
+            coverUrl={review.imgUrl}
+            title={review.title}
+            authorName={review.authorName}
+            publishTime={review.publishTime}
+          />
+        );
+      })}
+    </GeneralContainer>
+  );
+};
 export default PopularReviews;
