af0a49f1b9cffdc8057f125d96b393b2314bba6e
RoRobinh98 <houjiazhe98@163.com>
2023-02-18 23:16:12 +0700

    like_card_fix

--- src/pages/Home/components/MostLikes/MostLikesCom/LikeCard.tsx
+++ src/pages/Home/components/MostLikes/MostLikesCom/LikeCard.tsx
@@ -1,15 +1,24 @@
-import { Box, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
+import FavoriteRoundedIcon from '@mui/icons-material/FavoriteRounded';
+import ChatRoundedIcon from '@mui/icons-material/ChatRounded';
 import React from 'react';
-// import style from './index.module.css';
+import style from './index.module.css';
 
 // @ts-ignore
 const LikeCard = () => (
-  <div className="LikeCard">
-    This is like card
-    <div className="articleCover">This is cover</div>
-    <Typography className="articleTitle">This is article title</Typography>
-    <Box className="commentNumber">This is comment number</Box>
-    <Box className="likeNumber">This is like number</Box>
+  <div className={style.likeCard}>
+    <div className={style.cover}>Put the cover</div>
+    <div className="articleOverview">
+      <Typography className="articleTitle">Hogwarts Legacy: How to Open Eye Chests</Typography>
+      <div className="comment">
+        <ChatRoundedIcon className={style.comment_icon}>This is comment number</ChatRoundedIcon>
+        <Typography className="commentNum">2.6K</Typography>
+      </div>
+    </div>
+    <div className={style.liked}>
+      <FavoriteRoundedIcon className={style.heart}>This is cover</FavoriteRoundedIcon>
+      <Typography className="LikedNum">10,311</Typography>
+    </div>
   </div>
 );
 
