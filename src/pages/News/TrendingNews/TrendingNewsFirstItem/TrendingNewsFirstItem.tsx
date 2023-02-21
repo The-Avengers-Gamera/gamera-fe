a8a7fdf0e4abcc524c1e241696b28a85c615f885
JackNeedFocus <Jack.z.deng@outlook.com>
2023-02-21 20:50:39 +1030

    finish the basic layout of the trending news section

--- src/pages/News/TrendingNews/TrendingNewsFirstItem/TrendingNewsFirstItem.tsx
+++ src/pages/News/TrendingNews/TrendingNewsFirstItem/TrendingNewsFirstItem.tsx
@@ -1,4 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  position: relative;
+  width: 350px;
+  min-width: 350px;
+  height: 370px;
+  margin: 60px 20px 0;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+
+  .news-title-background {
+    position: absolute;
+    bottom: 0;
+    background-color: rgba(22, 24, 30, 0.7);
+    padding: 0 20px;
+    height: 80px;
+  }
+  .news-title-text {
+    margin-top: 15px;
+    font-weight: bold;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2; /* number of lines to show */
+    line-clamp: 2;
+    -webkit-box-orient: vertical;
+  }
+`;
 
 interface Props {
   news: {
@@ -9,14 +41,16 @@ interface Props {
 
 const TrendingNewsFirstItem = ({ news }: Props) => {
   return (
-    <div>
+    <Container>
       <img
         src={news?.coverUrl}
         alt="news cover"
         srcSet=""
       />
-      <p>{news?.title}</p>
-    </div>
+      <div className="news-title-background">
+        <p className="news-title-text">{news?.title}</p>
+      </div>
+    </Container>
   );
 };
 
--- src/pages/News/TrendingNews/TrendingNewsItem/TrendingNewsItem.tsx
+++ src/pages/News/TrendingNews/TrendingNewsItem/TrendingNewsItem.tsx
@@ -1,4 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Card = styled.div`
+  width: 313px;
+  flex-shrink: 0;
+
+  .title {
+    margin-top: 15px;
+    font-weight: bold;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2; /* number of lines to show */
+    line-clamp: 2;
+    -webkit-box-orient: vertical;
+  }
+
+  .comment-like {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
 
 interface Props {
   news: {
@@ -6,17 +28,19 @@ interface Props {
     commentCount: number;
     likeCount: number;
   };
+  order: string;
 }
 
-const TrendingNewsItem = ({ news }: Props) => {
+const TrendingNewsItem = ({ news, order }: Props) => {
   return (
-    <div>
-      <div>{news.title}</div>
-      <div>
+    <Card>
+      <div>{order}</div>
+      <div className="title">{news.title}</div>
+      <div className="comment-like">
         <span>{news.commentCount}</span>
         <span>{news.likeCount}</span>
       </div>
-    </div>
+    </Card>
   );
 };
 
