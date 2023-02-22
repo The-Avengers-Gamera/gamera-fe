44780db762d975d96cb3f5cb0d80fa0631a84260
XUeshuo Li <spoonlee24k@gmail.com>
2023-02-22 15:06:51 +1100

    move ReviewPage from page to component folder

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -9,7 +9,7 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
-      'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', 'Poppins', 'Gen Jyuu Gothic Monospace', sans-serif;
+      'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
