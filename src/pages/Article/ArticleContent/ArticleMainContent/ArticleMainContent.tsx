e1cb85b21fbdec13df8cf8ea6a953b7bdec412d5
JackNeedFocus <Jack.z.deng@outlook.com>
2023-03-03 21:14:52 +1030

    Finished the article content component

---
+++ src/pages/Article/ArticleContent/ArticleMainContent/ArticleMainContent.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import parse from 'html-react-parser';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 75%;
+  margin-bottom: 15px;
+  p {
+    font-size: 20px;
+  }
+
+  img {
+    width: 110%;
+    margin-bottom: 35px;
+    border-radius: 6px;
+  }
+`;
+
+interface Props {
+  mainContent: string;
+}
+
+const ArticleMainContent = ({ mainContent }: Props) => {
+  return <Container>{parse(mainContent)}</Container>;
+};
+
+export default ArticleMainContent;
