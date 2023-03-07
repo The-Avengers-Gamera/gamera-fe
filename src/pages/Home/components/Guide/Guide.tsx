495d9af691f6126a06e483f0cf5625a03588d637
Yvie Wei <yingyuwei0411@gmail.com>
2023-03-07 19:21:44 +1030

    Renew the 5-steps on GF-331

--- src/pages/Home/components/Guide/Guide.tsx
+++ src/pages/Home/components/Guide/Guide.tsx
@@ -1,14 +1,48 @@
 import { Box, Typography } from '@mui/material';
 import React from 'react';
+import banner from './assets/banner.png';
 import style from './index.module.css';
 
 const Guide = () => (
   <Box className={style.container}>
     <Box className={`${style.image} ${style.imageLeft}`} />
     <Box className={style.giudeContainer}>
-      <Typography variant="h6">Lorem ipsum dolor sit amet consectetur.</Typography>
+      <Typography
+        variant="h6"
+        sx={{
+          fontFamily: 'Alumni Sans, sans-serif',
+          fontSize: '28px',
+          fontWeight: 700,
+          paddingBottom: '30px',
+        }}
+      >
+        SMOOTH TO JOIN & COMMENT
+      </Typography>
+      <Box className={`${style.bannerContainer}`}>
+        <img
+          className={`${style.banner}`}
+          src={banner}
+          alt="banner"
+        />
+      </Box>
       <Box sx={{ mt: 2 }}>
-        <Typography variant="h6"> 1 ———— 2 ———— 3 ———— 4 ———— 5</Typography>
+        <Typography
+          variant="h6"
+          sx={{
+            display: 'flex',
+            gap: 24,
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontFamily: 'Alumni Sans, sans-serif',
+            fontSize: '18px',
+          }}
+        >
+          <span>REGISTER</span>
+          <span>NEWS</span>
+          <span>REVIEWS</span>
+          <span>COMMENTS</span>
+          <span>SHARE</span>
+        </Typography>
       </Box>
     </Box>
     <Box className={`${style.image} ${style.imageRight}`} />
