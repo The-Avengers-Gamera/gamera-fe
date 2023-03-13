cce92bde2d783e24c914b26fa93f4af287c4aa20
JackNeedFocus <Jack.z.deng@outlook.com>
2023-03-13 17:02:13 +1030

    feat: finish the integration of article

--- src/interfaces/article.d.ts
+++ src/interfaces/article.d.ts
@@ -1,17 +1,21 @@
 import { EArticleType } from '@/constants/article';
 import { IGame } from './game';
 import { IUser } from './user';
+import { ITagSlim } from './tag';
+import { ICommentSlim } from './comment';
 
 export interface IArticle {
   id: number;
   game?: IGame;
   user?: IUser;
   coverImgUrl: string;
+  tagList: ITagSlim[];
   title: string;
   text: string;
   type: EArticleType;
   createdTime: string;
   updatedTime: string;
+  commentList: IComment[];
 }
 
 export interface IArticlePut {
---
+++ src/interfaces/tag.d.ts
@@ -0,0 +1,4 @@
+export interface ITagSlim {
+  id: number;
+  name: string;
+}
