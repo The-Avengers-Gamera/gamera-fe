66567071904cc2650d74372ef20811ef7aeedabb
Yvie Wei <yingyuwei0411@gmail.com>
2023-03-15 20:50:49 +1030

    Refine the guide

--- src/pages/Home/utils/useFont.ts
+++ src/pages/Home/utils/useFont.ts
@@ -4,22 +4,23 @@ const titleFont = {
   fontSize: '28px',
 };
 
+// For '7 Days and view all'
 const secTitleFont = {
   fontFamily: 'Alumni Sans',
   fontWeight: 'Bold',
   fontSize: '20px',
 };
 
-const otherFont = {
-  fontFamily: 'Inter',
-  fontWeight: 'Regular',
-  fontSize: '14px',
-};
-
 const titleOfCards = {
   fontFamily: 'Inter',
   fontWeight: 'Bold',
   fontSize: '16px',
+};
+
+const otherFont = {
+  fontFamily: 'Inter',
+  fontWeight: 'Regular',
+  fontSize: '14px',
 };
 
 export { titleFont, otherFont, secTitleFont, titleOfCards };
