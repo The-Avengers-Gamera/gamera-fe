b61b094c85039a6de5128ccb5bf2179614e7e578
weited <weitedai.dev@gmail.com>
2023-03-20 17:30:06 +1100

    GF-457 config: update project package tool (#70)

--- src/pages/Article/Article.tsx
+++ src/pages/Article/Article.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import mockArticle from './mockArticle';
 import ArticleContent from './ArticleContent/ArticleContent';
