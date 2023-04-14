e5d3467417344436c594c3b7628cd5484d09e832
Kelozheng <100518138+Kelozheng@users.noreply.github.com>
2023-04-14 23:13:44 +1000

    GF 516: integrate slids

--- src/pages/Home/components/Carousel/Carousel.tsx
+++ src/pages/Home/components/Carousel/Carousel.tsx
@@ -1,47 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import style from './index.module.css';
+import { getPopularReviews } from '@/services/article';
+import { IArticleCard } from '@/interfaces/article';
 
 interface ICarouselProps {
   interval?: number;
 }
 
-const images = [
-  {
-    src: 'https://static0.gamerantimages.com/wordpress/wp-content/uploads/2020/11/Super-Mario-Odyssey.jpg',
-    name: 'Super Mario Odyssey',
-    url: 'https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/HEAD/docs/rules/anchor-is-valid.md',
-  },
-  {
-    src: 'https://static0.gamerantimages.com/wordpress/wp-content/uploads/2020/11/Super-Mario-World.jpg',
-    name: 'Super Mario World',
-    url: 'https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/HEAD/docs/rules/anchor-is-valid.md',
-  },
-  {
-    src: 'https://static0.gamerantimages.com/wordpress/wp-content/uploads/2020/11/Mario-Kart-DS.jpg',
-    name: 'Mario Kart DS',
-    url: 'https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/HEAD/docs/rules/anchor-is-valid.md',
-  },
-  {
-    src: 'https://static0.gamerantimages.com/wordpress/wp-content/uploads/2020/11/New-Super-Mario-Bros.-Wii.jpg',
-    name: 'New Super Mario Bros. Wii',
-    url: 'https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/HEAD/docs/rules/anchor-is-valid.md',
-  },
-  {
-    src: 'https://static0.gamerantimages.com/wordpress/wp-content/uploads/2020/11/Mario-Kart-8-Deluxe.jpg',
-    name: 'Mario Kart 8 Deluxe',
-    url: 'https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/HEAD/docs/rules/anchor-is-valid.md',
-  },
-  {
-    src: 'https://static0.gamerantimages.com/wordpress/wp-content/uploads/2020/11/New-Super-Mario-Bros..jpg',
-    name: 'New Super Mario Bros.',
-    url: 'https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/HEAD/docs/rules/anchor-is-valid.md',
-  },
-];
-
 const Carousel: React.FC<ICarouselProps> = ({ interval = 5000 }) => {
-  const [currentSlide, setCurrentSlide] = useState<{ src: string; name: string; url: string }>(
-    images[0]
-  );
+  const [images, setImages] = useState<IArticleCard[]>([]);
+  const [currentSlide, setCurrentSlide] = useState<IArticleCard>(images[0]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [msg, setMsg] = useState<string>('Loading...');
+
+  useEffect(() => {
+    const getImages = async () => {
+      await getPopularReviews()
+        .then((res) => {
+          setImages(res.data.data);
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          setMsg(err.error);
+        });
+    };
+    getImages();
+  }, []);
+
+  useEffect(() => {
+    setCurrentSlide(images[0]);
+  }, [images]);
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -66,30 +54,31 @@ const Carousel: React.FC<ICarouselProps> = ({ interval = 5000 }) => {
 
   return (
     <div className={style.container}>
-      {images.map((image) => (
-        <div key={image.src}>
-          <a href={image.url}>
-            <img
-              src={image.src}
-              alt={image.name}
-              style={{
-                opacity: image === currentSlide ? 1 : 0,
-              }}
-              className={style.carouselImg}
-            />
-          </a>
-
-          <div className={style.contentBackground} />
-          <div
-            className={style.title}
-            style={{
-              opacity: image === currentSlide ? 1 : 0,
-            }}
-          >
-            <a href={image.url}> {image.name}</a>
-          </div>
-        </div>
-      ))}
+      {isLoading
+        ? [msg]
+        : images.map((image) => (
+            <div key={image.id}>
+              <a href={image.coverImgUrl}>
+                <img
+                  src={image.coverImgUrl}
+                  alt={image.title}
+                  style={{
+                    opacity: image === currentSlide ? 1 : 0,
+                  }}
+                  className={style.carouselImg}
+                />
+              </a>
+              <div className={style.contentBackground} />
+              <div
+                className={style.title}
+                style={{
+                  opacity: image === currentSlide ? 1 : 0,
+                }}
+              >
+                <a href={image.coverImgUrl}>{image.title}</a>
+              </div>
+            </div>
+          ))}
       <div className={style.carouselBtnContainer}>
         <button
           type="button"
