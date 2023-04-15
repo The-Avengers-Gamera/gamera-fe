f477f2e4dcba393ed8548391c365401cf6720f99
Xiangcheng <769358935@qq.com>
2023-04-16 08:31:28 +0930

    GF-507-adjusted load more button position (#98)
    Co-authored-by: Jennie Zhu <37766866+JennieZhu29@users.noreply.github.com>

--- src/components/LoadMore/LoadMore.tsx
+++ src/components/LoadMore/LoadMore.tsx
@@ -1,33 +1,39 @@
 import styled from 'styled-components';
+import React from 'react';
+import { IArticleCard } from '@/interfaces/article';
+import { IPage } from '@/interfaces/page';
 
 const Container = styled.div`
+  margin-top: 1rem;
   display: flex;
   justify-content: center;
   align-items: center;
   width: 960px;
-
   padding-bottom: 25px;
-
   & button {
     padding: 8px 16px;
     border-radius: 8px;
-
     border: none;
-
     background-color: ${({ theme }) => theme.color.primary};
     color: #3d3d3d; // TODO: manage the color using theme instead
-
     cursor: pointer;
   }
 `;
 
 type LoadMoreProps = {
   setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
+  setIsAtTheEnd: React.Dispatch<React.SetStateAction<boolean>>;
+  currentPage: number;
+  totalPages: number;
 };
 
-const LoadMore = ({ setCurrentPage }: LoadMoreProps) => {
+const LoadMore = ({ setCurrentPage, currentPage, totalPages, setIsAtTheEnd }: LoadMoreProps) => {
   const changePage = () => {
-    setCurrentPage((prevState) => prevState + 1);
+    if (currentPage < totalPages) {
+      setCurrentPage((prevState) => prevState + 1);
+      return;
+    }
+    setIsAtTheEnd(true);
   };
   return (
     <Container>
